0edf50fd689946c4949bb8480367f884268e1968
Ricardo Camargo Taricio <ricardotaricio@gmail.com>
2024-07-29 19:34:22 -0300

    Bugfix lazyloading exception

--- src/main/java/me/dio/cardapio/entity/Cardapio.java
+++ src/main/java/me/dio/cardapio/entity/Cardapio.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -20,7 +21,7 @@ public class Cardapio {
 	
 	private String nome;
 	
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(
 			name="cardapio_produto",
 		    joinColumns=
