2f100489df95af708a582761aedb094145955420
Ricardo Camargo Taricio <ricardotaricio@gmail.com>
2024-07-29 16:29:39 -0300

    rename test package

--- src/test/java/me/dio/cardapio/ApplicationTests.java
+++ src/test/java/me/dio/cardapio/ApplicationTests.java
@@ -1,10 +1,10 @@
-package dio.lab_padroes_projeto_spring;
+package me.dio.cardapio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LabPadroesProjetoSpringApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
