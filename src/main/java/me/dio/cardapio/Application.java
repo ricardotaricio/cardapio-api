7ddfd412a859c82752e3bf31c61b92b7a6857f3f
Ricardo Camargo Taricio <ricardotaricio@gmail.com>
2024-07-29 20:56:22 -0300

    Bugfix CORS

--- src/main/java/me/dio/cardapio/Application.java
+++ src/main/java/me/dio/cardapio/Application.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
-//@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class Application {
 
 	public static void main(String[] args) {
